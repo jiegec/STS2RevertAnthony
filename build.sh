881e65e92ed263ffff9aa7f3ba09c09cad0a3e86
Jiajie Chen <c@jia.je>
2026-04-18 21:46:21 +0800

    Generate zip package

--- build.sh
+++ build.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
-set -x
+set -x -e
 
 OS="$(uname -s)"
 
 case "$OS" in
     Linux*)
-        cp ~/.steam/steam/steamapps/common/Slay\ the\ Spire\ 2/data_sts2_linuxbsd_x86_64/sts2.dll .
-        cp ~/.steam/steam/steamapps/common/Slay\ the\ Spire\ 2/data_sts2_linuxbsd_x86_64/0Harmony.dll .
-        ../Godot_v4.5.1-stable_mono_linux_x86_64/Godot_v4.5.1-stable_mono_linux.x86_64 --build-solutions --quit --headless
-        rm -rf dist
-        mkdir -p dist
-        cp ./.godot/mono/temp/bin/Debug/RevertAnthony.dll dist/
-        ../Godot_v4.5.1-stable_mono_linux_x86_64/Godot_v4.5.1-stable_mono_linux.x86_64 --export-pack "Windows Desktop" dist/RevertAnthony.pck --headless
-        cp RevertAnthony.json dist/RevertAnthony.json
+        STS2_DLL="$HOME/.steam/steam/steamapps/common/Slay the Spire 2/data_sts2_linuxbsd_x86_64/sts2.dll"
+        HARMONY_DLL="$HOME/.steam/steam/steamapps/common/Slay the Spire 2/data_sts2_linuxbsd_x86_64/0Harmony.dll"
+        GODOT="../Godot_v4.5.1-stable_mono_linux_x86_64/Godot_v4.5.1-stable_mono_linux.x86_64"
         ;;
     Darwin*)
-        cp ~/Library/Application\ Support/steam/steamapps/common/Slay\ the\ Spire\ 2/SlayTheSpire2.app/Contents/Resources/data_sts2_macos_arm64/sts2.dll .
-        cp ~/Library/Application\ Support/steam/steamapps/common/Slay\ the\ Spire\ 2/SlayTheSpire2.app/Contents/Resources/data_sts2_macos_arm64/0Harmony.dll .
-        /Applications/Godot_mono.app/Contents/MacOS/Godot --build-solutions --quit --headless
-        rm -rf dist
-        mkdir -p dist
-        cp ./.godot/mono/temp/bin/Debug/RevertAnthony.dll dist/
-        /Applications/Godot_mono.app/Contents/MacOS/Godot --export-pack "Windows Desktop" dist/RevertAnthony.pck --headless
-        cp RevertAnthony.json dist/RevertAnthony.json
+        STS2_DLL="$HOME/Library/Application Support/steam/steamapps/common/Slay the Spire 2/SlayTheSpire2.app/Contents/Resources/data_sts2_macos_arm64/sts2.dll"
+        HARMONY_DLL="$HOME/Library/Application Support/steam/steamapps/common/Slay the Spire 2/SlayTheSpire2.app/Contents/Resources/data_sts2_macos_arm64/sts2.dll"
+        GODOT="/Applications/Godot_mono.app/Contents/MacOS/Godot"
         ;;
     *)
         echo "Unknown operating system: $OS"
@@ -30,3 +20,15 @@ case "$OS" in
         ;;
 esac
 
+cp "$STS2_DLL" .
+cp "$HARMONY_DLL" .
+$GODOT --build-solutions --quit --headless --verbose
+rm -rf dist
+mkdir -p dist
+cp ./.godot/mono/temp/bin/Debug/RevertAnthony.dll dist/
+$GODOT --export-pack "Windows Desktop" dist/RevertAnthony.pck --headless
+cp RevertAnthony.json dist/RevertAnthony.json
+
+VERSION=$(jq -r ".version" RevertAnthony.json)
+rm -f RevertAnthony-v$VERSION.zip
+cd dist && zip -r ../RevertAnthony-v$VERSION.zip .
