5a68372d6cbff23140b3af07809287a913d44ee5
Jiajie Chen <c@jia.je>
2026-05-23 11:05:46 +0800

    chore: add code diff from v0.103.2 against latest beta

--- diff.sh
+++ diff.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-diff -u10 -r -N $1/MegaCrit/Sts2/Core/Models/Cards $2/MegaCrit/Sts2/Core/Models/Cards > code-v0.99.1-v0.103.2-cards.diff
-sed -i "s!$1!v0.99.1!g" code-v0.99.1-v0.103.2-cards.diff
-sed -i "s!$2!v0.103.2!g" code-v0.99.1-v0.103.2-cards.diff
-sed -E -i "s!\t[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}!!g" code-v0.99.1-v0.103.2-cards.diff
+diff -u10 -r -N $1/MegaCrit/Sts2/Core/Models/Cards $2/MegaCrit/Sts2/Core/Models/Cards > code-$3-$4-cards.diff
+sed -i "s!$1!$3!g" code-$3-$4-cards.diff
+sed -i "s!$2!$4!g" code-$3-$4-cards.diff
+sed -E -i "s!\t[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}!!g" code-$3-$4-cards.diff
 
-diff -u10 -r -N $1/MegaCrit/Sts2/Core/Models/Powers $2/MegaCrit/Sts2/Core/Models/powers > code-v0.99.1-v0.103.2-powers.diff
-sed -i "s!$1!v0.99.1!g" code-v0.99.1-v0.103.2-powers.diff
-sed -i "s!$2!v0.103.2!g" code-v0.99.1-v0.103.2-powers.diff
-sed -E -i "s!\t[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}!!g" code-v0.99.1-v0.103.2-powers.diff
+diff -u10 -r -N $1/MegaCrit/Sts2/Core/Models/Powers $2/MegaCrit/Sts2/Core/Models/powers > code-$3-$4-powers.diff
+sed -i "s!$1!$3!g" code-$3-$4-powers.diff
+sed -i "s!$2!$4!g" code-$3-$4-powers.diff
+sed -E -i "s!\t[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}!!g" code-$3-$4-powers.diff
 
-diff -u10 -r -N $1/MegaCrit/Sts2/Core/Models/Monsters $2/MegaCrit/Sts2/Core/Models/monsters > code-v0.99.1-v0.103.2-monsters.diff
-sed -i "s!$1!v0.99.1!g" code-v0.99.1-v0.103.2-monsters.diff
-sed -i "s!$2!v0.103.2!g" code-v0.99.1-v0.103.2-monsters.diff
-sed -E -i "s!\t[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}!!g" code-v0.99.1-v0.103.2-monsters.diff
+diff -u10 -r -N $1/MegaCrit/Sts2/Core/Models/Monsters $2/MegaCrit/Sts2/Core/Models/monsters > code-$3-$4-monsters.diff
+sed -i "s!$1!$3!g" code-$3-$4-monsters.diff
+sed -i "s!$2!$4!g" code-$3-$4-monsters.diff
+sed -E -i "s!\t[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}!!g" code-$3-$4-monsters.diff
